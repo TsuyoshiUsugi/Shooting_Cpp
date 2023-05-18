7b721a96c7c0669ac5a22779d46fa0b18f2fa58c
TsuyosiUsugi <qesulive@gmail.com>
2023-05-19 00:12:11 +0900

    [add] 敵のクラスを追加

---
+++ Enemy.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../dxlib_ext/dxlib_ext.h"
+
+class Enemy
+{
+public:
+	int enemy_gpc_hdl_ = 0;
+	int hp_ = 10;
+	int speed_ = 10;
+	tnl::Vector3 pos_ = {0, 0, 0};
+
+	void attack();
+};
+
--- gm_main.cpp
+++ gm_main.cpp
@@ -3,6 +3,7 @@
 #include "../dxlib_ext/dxlib_ext.h"
 #include "gm_main.h"
 #include "Player.h"
+#include "Enemy.h"
 
 //外側の壁用の値
 RECT rect;
@@ -11,6 +12,7 @@ const tnl::Vector3 outer_box_size = {900, 700, 0};
 
 //playerの設定
 Player player;
+Enemy enemy;
 
 //------------------------------------------------------------------------------------------------------------
 // ゲーム起動時に１度だけ実行されます
@@ -21,6 +23,11 @@ void gameStart(){
 	//GetClientRect(GetMainWindowHandle(), &rect);
 	
 	player.player_gpc_hdl_ = LoadGraph("graphics/Player.png");
+	enemy.enemy_gpc_hdl_ = LoadGraph("graphics/Enemy.bmp");
+}
+
+void checkPos()
+{
 
 }
 
@@ -28,16 +35,18 @@ void gameStart(){
 // 毎フレーム実行されます
 void gameMain(float delta_time) {
 	//外枠を表示する
+	enemy.pos_.y += enemy.speed_;
 	DrawBoxEx(outer_box_pos, outer_box_size.x, outer_box_size.y, false, -1);
+
+	//敵移動処理
+	DrawGraph(enemy.pos_.x, enemy.pos_.y, enemy.enemy_gpc_hdl_, false);
 
 	//playerの移動処理
 	if (tnl::Input::IsKeyDown(eKeys::KB_LEFT)) player.current_player_pos_.x -= player.speed_;
 	if (tnl::Input::IsKeyDown(eKeys::KB_RIGHT)) player.current_player_pos_.x += player.speed_;
-
 	if (player.current_player_pos_.x > outer_box_pos.x + outer_box_size.x / 2 - 110) {
 		player.current_player_pos_.x = outer_box_pos.x + outer_box_size.x / 2 - 110;
 	}
-
 	if (player.current_player_pos_.x < outer_box_pos.x - outer_box_size.x / 2 + 10) {
 		player.current_player_pos_.x = outer_box_pos.x - outer_box_size.x / 2 + 10;
 	}
